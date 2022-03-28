bd18ac8106ffb736060c6c3b8646c526a6d59420
SydH8 <sydneyhuang8@gmail.com>
2022-03-27 20:04:30 -0700

    update 3/27

---
+++ jsindex.js
@@ -0,0 +1,68 @@
+
+$(".cardAnimate").hover(function() {
+    $(this).animate({
+        height: "+=20px",
+        width: "+=30px"}, 300);
+    
+}, function() {
+    $(this).animate({
+        height: "-=20px",
+        width: "-=30px"}, 300);
+});
+
+/*$("#card1").on("mouseenter", function() {
+    $("#card1").animate({
+        height: "+=20px",
+        width: "+=30px"}, 300);
+    $("#img1").animate({
+        height: "+=20px",
+        width: "+=30px"}, 300);
+});
+$("#card1").on("mouseleave", function() {
+    $("#card1").animate({
+        height: "-=20px",
+        width: "-=30px"}, 300);
+    $("#img1").animate({
+        height: "-=20px",
+        width: "-=30px"}, 300);
+});
+
+
+$("#card2").on("mouseenter", function() {
+    $(this).animate({
+        height: "+=20px",
+        width: "+=30px"}, 300);
+    $("#img2").animate({
+        height: "+=20px",
+        width: "+=30px"}, 300);
+});
+$("#card2").on("mouseleave", function() {
+    $(this).animate({
+        height: "-=20px",
+        width: "-=30px"}, 300);
+    $("#img2").animate({
+        height: "-=20px",
+        width: "-=30px"}, 300);
+});
+
+
+$("#card3").on("mouseenter", function() {
+    $(this).animate({
+        height: "+=20px",
+        width: "+=30px"}, 300);
+    $("#img3").animate({
+        height: "+=20px",
+        width: "+=30px"}, 300);
+});
+
+$("#card3").on("mouseleave", function() {
+    $(this).animate({
+        height: "-=20px",
+        width: "-=30px"}, 300);
+    $("#img3").animate({
+        height: "-=20px",
+        width: "-=30px"}, 300);
+});
+*/
+
+
